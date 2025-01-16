daaa26157e39738f97bd2e1d93681d21e04aafb9
Neelam06 <42576374+Neelam06@users.noreply.github.com>
2025-01-16 08:14:51 +0530

    checking_gitcommit

--- src/Task01_PrinfExample.java
+++ src/Task01_PrinfExample.java
@@ -2,6 +2,7 @@ public class Task01_PrinfExample {
 
     public static void main(String[] args) {
         int a = 9;
+        System.out.printf("/n","Table of 9");
         System.out.printf("%d%n", a);
         System.out.printf("%d%n", a*2);
         System.out.printf("%d%n", a*3);
