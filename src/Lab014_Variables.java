3edaf21cb3652d8fb4ec5320d00441b623a07c4e
Neelam06 <42576374+Neelam06@users.noreply.github.com>
2025-01-16 07:56:54 +0530

    myfirstcommit

---
+++ src/Lab014_Variables.java
@@ -0,0 +1,6 @@
+public class Lab014_Variables {
+    public static void main(String[] args) {
+        int a = 9;
+
+    }
+}
---
+++ src/Task01_PrinfExample.java
@@ -0,0 +1,16 @@
+public class Task01_PrinfExample {
+
+    public static void main(String[] args) {
+        int a = 9;
+        System.out.printf("%d%n", a);
+        System.out.printf("%d%n", a*2);
+        System.out.printf("%d%n", a*3);
+        System.out.printf("%d%n", a*4);
+        System.out.printf("%d%n", a*5);
+        System.out.printf("%d%n", a*6);
+        System.out.printf("%d%n", a*7);
+        System.out.printf("%d%n", a*8);
+        System.out.printf("%d%n", a*9);
+        System.out.printf("%d%n", a*10);
+    }
+}
